4c00f3b8619afb398c5ecf6fc8eb3145fab2f437
aldoushux503 <elnikovnikita4@gmail.com>
2023-06-05 19:17:58 +0200

    Added package entity with class for github repositories and some changes at service class

---
+++ src/main/java/com/example/githubapi/entity/GitHubRepository.java
@@ -0,0 +1,22 @@
+package com.example.githubapi.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GitHubRepository {
+
+    private String name;
+
+
+    private boolean fork;
+}
+
--- src/main/java/com/example/githubapi/service/impl/GitRepoServiceImpl.java
+++ src/main/java/com/example/githubapi/service/impl/GitRepoServiceImpl.java
@@ -1,15 +1,33 @@
 package com.example.githubapi.service.impl;
 
 
+import com.example.githubapi.entity.GitHubRepository;
 import com.example.githubapi.service.GitRepoService;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class GitRepoServiceImpl implements GitRepoService {
     @Override
     public List<?> getRepositories(String username) {
-        return null;
+        String apiUrl = "https://api.github.com/users/" + username + "/repos";
+
+        RestTemplate restTemplate = new RestTemplate();
+        GitHubRepository[] repositories = restTemplate.getForObject(apiUrl, GitHubRepository[].class);
+
+        List<GitHubRepository> filteredRepositories = new ArrayList<>();
+
+        if (repositories != null) {
+            for (GitHubRepository repository : repositories) {
+                if (!repository.isFork()) {
+
+                }
+            }
+        }
+
+        return filteredRepositories;
     }
 }
