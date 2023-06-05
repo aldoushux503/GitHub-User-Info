54b42e1859246c44a413b4116523018d8ae89e4a
aldoushux503 <elnikovnikita4@gmail.com>
2023-06-05 18:44:10 +0200

    Deleted github dependency, renamed and moved controller to package

---
+++ src/main/java/com/example/githubapi/controller/GitHubRepoController.java
@@ -0,0 +1,11 @@
+package com.example.githubapi.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/repositories")
+public class GitHubRepoController {
+
+}
