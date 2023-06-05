593efe099bdf27df4a730cda7bcc15e3d8e70fb7
aldoushux503 <elnikovnikita4@gmail.com>
2023-06-05 19:01:04 +0200

    Added service dependency to controller class and provided endpoint

--- src/main/java/com/example/githubapi/controller/GitRepoController.java
+++ src/main/java/com/example/githubapi/controller/GitRepoController.java
@@ -1,11 +1,31 @@
 package com.example.githubapi.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.githubapi.service.GitRepoService;
+import com.example.githubapi.service.impl.GitRepoServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/repositories")
 public class GitRepoController {
+
+    private final GitRepoService repoService;
+
+    public GitRepoController(GitRepoService repoService) {
+        this.repoService = repoService;
+    }
+
+    @GetMapping("/{username}")
+    public ResponseEntity<?> getUserRepositories(
+            @PathVariable String username,
+            @RequestHeader("Accept") String acceptHeader
+    ) {
+
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
 
 }
--- src/main/java/com/example/githubapi/service/impl/GitRepoServiceImpl.java
+++ src/main/java/com/example/githubapi/service/impl/GitRepoServiceImpl.java
@@ -2,6 +2,8 @@ package com.example.githubapi.service.impl;
 
 
 import com.example.githubapi.service.GitRepoService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class GitRepoServiceImpl implements GitRepoService {
 }
