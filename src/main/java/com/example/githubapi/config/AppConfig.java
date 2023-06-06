bc833e00976e91f477a3520b15c95828b9c20af9
aldoushux503 <elnikovnikita4@gmail.com>
2023-06-06 18:57:41 +0200

    Replaced code at service and config with WebClient

--- src/main/java/com/example/githubapi/config/AppConfig.java
+++ src/main/java/com/example/githubapi/config/AppConfig.java
@@ -2,12 +2,18 @@ package com.example.githubapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class AppConfig {
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public WebClient webClient() {
+        return WebClient.builder()
+                .baseUrl("https://api.github.com")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
     }
 }
--- src/main/java/com/example/githubapi/service/impl/GitRepoServiceImpl.java
+++ src/main/java/com/example/githubapi/service/impl/GitRepoServiceImpl.java
@@ -4,58 +4,46 @@ package com.example.githubapi.service.impl;
 import com.example.githubapi.entity.Branch;
 import com.example.githubapi.entity.GitHubRepository;
 import com.example.githubapi.service.GitRepoService;
+import org.reactivestreams.Publisher;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class GitRepoServiceImpl implements GitRepoService {
 
-    private final RestTemplate restTemplate;
+    private final WebClient webClient;
 
     @Autowired
-    public GitRepoServiceImpl(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
+    public GitRepoServiceImpl(WebClient webClient) {
+        this.webClient = webClient;
     }
 
     @Override
     public List<GitHubRepository> getRepositories(String username) {
-        String apiUrl = "https://api.github.com/users/" + username + "/repos";
-
-        GitHubRepository[] repositories;
-        try {
-            repositories = restTemplate.getForObject(apiUrl, GitHubRepository[].class);
-        } catch (HttpClientErrorException e) {
-            throw new HttpClientErrorException(HttpStatus.NOT_FOUND, "Not Found");
-        }
-
-
-        List<GitHubRepository> filteredRepositories = new ArrayList<>();
-
-        if (repositories != null) {
-            for (GitHubRepository repository : repositories) {
-                if (!repository.isFork()) {
-                    GitHubRepository detailedRepository = getRepositoryDetails(repository);
-                    filteredRepositories.add(detailedRepository);
-                }
-            }
-        }
-
-        return filteredRepositories;
+        return webClient.get()
+                .uri("users/{username}/repos", username)
+                .retrieve()
+                .bodyToFlux(GitHubRepository.class)
+                .filter(e -> !e.isFork())
+                .flatMap(this::getRepositoryDetails)
+                .collectList()
+                .block();
     }
 
-    private GitHubRepository getRepositoryDetails(GitHubRepository repository) {
-        String apiUrl = "https://api.github.com/repos/" + repository.getFull_name() + "/branches";
-
-        Branch[] branches = restTemplate.getForObject(apiUrl, Branch[].class);
-        repository.setBranches(Arrays.asList(branches));
-
-        return repository;
+    private Mono<GitHubRepository> getRepositoryDetails(GitHubRepository repository) {
+        return webClient.get()
+                .uri("repos/{fullName}/branches", repository.getFull_name())
+                .retrieve()
+                .bodyToFlux(Branch.class)
+                .collectList()
+                .flatMap(branches -> {
+                    repository.setBranches(branches);
+                    return Mono.just(repository);
+                });
     }
+
 }
