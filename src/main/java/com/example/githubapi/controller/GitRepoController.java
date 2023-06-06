ef197f13be7c726b0b3e020cef93f1e64b00df02
aldoushux503 <elnikovnikita4@gmail.com>
2023-06-06 18:01:23 +0200

    Moved and rename exception response class, added exception handler and simplified the method at controller

--- src/main/java/com/example/githubapi/controller/GitRepoController.java
+++ src/main/java/com/example/githubapi/controller/GitRepoController.java
@@ -1,15 +1,11 @@
 package com.example.githubapi.controller;
 
-
-import com.example.githubapi.exception.ErrorResponse;
-import com.example.githubapi.exception.ExceptionResponse;
 import com.example.githubapi.service.GitRepoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.HttpClientErrorException;
 
 @RestController
 @RequestMapping("/repositories")
@@ -26,35 +22,15 @@ public class GitRepoController {
     public ResponseEntity<?> getUserRepositories(
             @PathVariable String username,
             @RequestHeader("Accept") String acceptHeader
-    ) {
+    ) throws HttpMediaTypeNotSupportedException, HttpMediaTypeNotAcceptableException {
         if ("application/json".equalsIgnoreCase(acceptHeader)) {
-            try {
-                // Call the GitHub service to retrieve repositories in JSON format
-                return ResponseEntity.ok(repoService.getRepositories(username));
-            }
-            catch (HttpClientErrorException e) {
-                // Return 404 response for non-existent GitHub user
-                return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                        .body(createErrorResponse(404, "Non-existent Github user"));
-            }
-        }  else if ("application/xml".equalsIgnoreCase(acceptHeader)) {
-            // Return 406 response for unsupported media type
-            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE)
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .body(createErrorResponse(406, "Not acceptable media type"));
+            // Call the GitHub service to retrieve repositories in JSON format
+            return ResponseEntity.ok(repoService.getRepositories(username));
+        } else if ("application/xml".equalsIgnoreCase(acceptHeader)) {
+            throw new HttpMediaTypeNotAcceptableException("Not acceptable media type");
         } else {
-            // Return 400 response for unsupported media type
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body(createErrorResponse(400, "Invalid Accept header"));
+            throw new HttpMediaTypeNotSupportedException("Invalid Accept header");
         }
-    }
-
-
-    private ExceptionResponse createErrorResponse(int status, String message) {
-        ExceptionResponse errorResponse = new ExceptionResponse();
-        errorResponse.setStatus(status);
-        errorResponse.setMessage(message);
-        return errorResponse;
     }
 
 }
--- src/main/java/com/example/githubapi/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/githubapi/exception/GlobalExceptionHandler.java
@@ -1,2 +1,47 @@
-package com.example.githubapi.exception;public class GlobalExceptionHandler {
+package com.example.githubapi.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpClientErrorException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(HttpClientErrorException.class)
+    public ResponseEntity<ExceptionResponse> handleNotFoundException(HttpClientErrorException ex) {
+        ExceptionResponse exceptionResponse = createErrorResponse(404, "Non-existent Github user");
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(exceptionResponse);
+    }
+
+    @ExceptionHandler(HttpMediaTypeNotAcceptableException.class)
+    public ResponseEntity<ExceptionResponse> handleMediaTypeNotAcceptableException(HttpMediaTypeNotAcceptableException ex) {
+        ExceptionResponse exceptionResponse = createErrorResponse(406, "Not acceptable media type");
+        return ResponseEntity
+                .status(HttpStatus.NOT_ACCEPTABLE)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(exceptionResponse);
+    }
+
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    public ResponseEntity<ExceptionResponse> handleMediaTypeNotSupportedException(HttpMediaTypeNotSupportedException ex) {
+        ExceptionResponse exceptionResponse = createErrorResponse(400, "Invalid Accept header");
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(exceptionResponse);
+    }
+
+    private ExceptionResponse createErrorResponse(int status, String message) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse();
+        exceptionResponse.setStatus(status);
+        exceptionResponse.setMessage(message);
+        return exceptionResponse;
+    }
 }
+
